357793d522f358e6f1596cab91718269f44b4746
axeon <23231269@qq.com>
2018-02-20 22:30:55 +0800

    修正ThreadPoolExecutor的使用

--- src/main/java/uw/task/TaskScheduler.java
+++ src/main/java/uw/task/TaskScheduler.java
@@ -1,29 +1,19 @@
 package uw.task;
 
-import java.util.Date;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpException;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageDeliveryMode;
-import org.springframework.amqp.core.MessagePostProcessor;
-import org.springframework.amqp.core.MessageProperties;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
+import org.springframework.amqp.core.*;
 import uw.task.conf.TaskMetaInfoManager;
 import uw.task.conf.TaskProperties;
 import uw.task.container.TaskRunnerContainer;
 import uw.task.util.GlobalSequenceManager;
 import uw.task.util.TaskMessageConverter;
+
+import java.util.Date;
+import java.util.concurrent.*;
 
 /**
  * 任务执行器。 通过调用此类，可以实现队列执行，RPC调用，本地调用等功能。
@@ -32,168 +22,163 @@ import uw.task.util.TaskMessageConverter;
  */
 public class TaskScheduler {
 
-	private static final Logger log = LoggerFactory.getLogger(TaskScheduler.class);
+    private static final Logger log = LoggerFactory.getLogger(TaskScheduler.class);
 
-	/**
-	 * Task配置文件
-	 */
-	private TaskProperties taskProperties;
+    /**
+     * Task配置文件
+     */
+    private TaskProperties taskProperties;
 
-	/**
-	 * rabbitTemplate模板.
-	 */
-	private AmqpTemplate rabbitTemplate;
+    /**
+     * rabbitTemplate模板.
+     */
+    private AmqpTemplate rabbitTemplate;
 
-	/**
-	 * 全局sequence序列，主要用于taskLog日志。
-	 */
-	private GlobalSequenceManager globalSequenceManager;
+    /**
+     * 全局sequence序列，主要用于taskLog日志。
+     */
+    private GlobalSequenceManager globalSequenceManager;
 
-	/**
-	 * 用于本地执行任务的taskConsumer。
-	 */
-	private TaskRunnerContainer taskRunnerContainer;
+    /**
+     * 用于本地执行任务的taskConsumer。
+     */
+    private TaskRunnerContainer taskRunnerContainer;
 
-	/**
-	 * rpc异步调用线程池
-	 */
-	private ExecutorService taskRpcService = null;
+    /**
+     * rpc异步调用线程池
+     */
+    private ExecutorService taskRpcService = null;
 
-	public TaskScheduler(TaskProperties taskProperties, AmqpTemplate rabbitTemplate,
-			TaskRunnerContainer taskRunnerContainer, GlobalSequenceManager globalSequenceManager) {
-		this.taskProperties = taskProperties;
-		this.rabbitTemplate = rabbitTemplate;
-		this.taskRunnerContainer = taskRunnerContainer;
-		this.globalSequenceManager = globalSequenceManager;
-		taskRpcService = new ThreadPoolExecutor(taskProperties.getTaskRpcMinThreadNum(),
-				taskProperties.getTaskRpcMaxThreadNum(), 20L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(),
-				new ThreadFactoryBuilder().setDaemon(true).setNameFormat("TaskRpc-%d").build());
-	}
+    public TaskScheduler(TaskProperties taskProperties, AmqpTemplate rabbitTemplate,
+                         TaskRunnerContainer taskRunnerContainer, GlobalSequenceManager globalSequenceManager) {
+        this.taskProperties = taskProperties;
+        this.rabbitTemplate = rabbitTemplate;
+        this.taskRunnerContainer = taskRunnerContainer;
+        this.globalSequenceManager = globalSequenceManager;
+        taskRpcService = new ThreadPoolExecutor(taskProperties.getTaskRpcMinThreadNum(),
+                taskProperties.getTaskRpcMaxThreadNum(), 20L, TimeUnit.SECONDS, new SynchronousQueue<>(),
+                new ThreadFactoryBuilder().setDaemon(true).setNameFormat("TaskRpc-%d").build(), new ThreadPoolExecutor.CallerRunsPolicy());
+    }
 
-	/**
-	 * 把任务发送到队列中
-	 *
-	 * @param taskdata
-	 *            任务数据
-	 */
-	public void sendToQueue(final TaskData<?, ?> taskData) {
-		taskData.setId(globalSequenceManager.nextId("task_runner_log"));
-		taskData.setQueueDate(new Date());
-		taskData.setRunType(TaskData.RUN_TYPE_GLOBAL);
-		String queue = TaskMetaInfoManager.getFitQueue(taskData);
-		rabbitTemplate.convertAndSend(queue, queue, taskData);
-	}
-	
-	
-	/**
-	 * 同步执行任务，可能会导致阻塞。
-	 *
-	 * @param runTarget
-	 *            目标主机配置名，如果没有，则为空
-	 * @param taskData
-	 *            任务数据
-	 * @return
-	 */
-	public <TP, RD> TaskData<TP, RD> runTask(final TaskData<TP, RD> taskData,
-			final TypeReference<TaskData<TP, RD>> typeRef) {
-		taskData.setId(globalSequenceManager.nextId("task_runner_log"));
-		taskData.setQueueDate(new Date());
+    /**
+     * 把任务发送到队列中
+     *
+     * @param taskData 任务数据
+     */
+    public void sendToQueue(final TaskData<?, ?> taskData) {
+        taskData.setId(globalSequenceManager.nextId("task_runner_log"));
+        taskData.setQueueDate(new Date());
+        taskData.setRunType(TaskData.RUN_TYPE_GLOBAL);
+        String queue = TaskMetaInfoManager.getFitQueue(taskData);
+        rabbitTemplate.convertAndSend(queue, queue, taskData);
+    }
 
-		// 当自动RPC，并且本地有runner，而且target匹配的时候，运行在本地模式下。
-		if (taskData.getRunType() == TaskData.RUN_TYPE_AUTO_RPC && TaskMetaInfoManager.checkRunnerRunLocal(taskData)) {
-			// 启动本地运行模式。
-			taskData.setRunType(TaskData.RUN_TYPE_LOCAL);
-		} else {
-			taskData.setRunType(TaskData.RUN_TYPE_GLOBAL_RPC);
-		}
 
-		if (taskData.getRunType() == TaskData.RUN_TYPE_LOCAL) {
-			taskRunnerContainer.process(taskData);
-			return taskData;
-		} else {
-			// 全局运行模式
-			String queue = TaskMetaInfoManager.getFitQueue(taskData);
-			@SuppressWarnings("unchecked")
-			TaskData<TP, RD> retdata = (TaskData<TP, RD>) rabbitTemplate.convertSendAndReceive(queue, queue, taskData,
-					new MessagePostProcessor() {
+    /**
+     * 同步执行任务，可能会导致阻塞。
+     *
+     * @param runTarget 目标主机配置名，如果没有，则为空
+     * @param taskData  任务数据
+     * @return
+     */
+    public <TP, RD> TaskData<TP, RD> runTask(final TaskData<TP, RD> taskData,
+                                             final TypeReference<TaskData<TP, RD>> typeRef) {
+        taskData.setId(globalSequenceManager.nextId("task_runner_log"));
+        taskData.setQueueDate(new Date());
 
-						@Override
-						public Message postProcessMessage(Message message) throws AmqpException {
-							MessageProperties mp = message.getMessageProperties();
-							mp.setPriority(10);
-							mp.setDeliveryMode(MessageDeliveryMode.NON_PERSISTENT);
-							mp.setExpiration("180000");
-							return message;
-						}
-					});
-			if (typeRef != null) {
-				try {
-					retdata = TaskMessageConverter.getTaskObjectMapper().convertValue(retdata, typeRef);
-				} catch (Exception e) {
-					log.error(e.getMessage(), e);
-				}
-			}
-			return retdata;
-		}
-	}
+        // 当自动RPC，并且本地有runner，而且target匹配的时候，运行在本地模式下。
+        if (taskData.getRunType() == TaskData.RUN_TYPE_AUTO_RPC && TaskMetaInfoManager.checkRunnerRunLocal(taskData)) {
+            // 启动本地运行模式。
+            taskData.setRunType(TaskData.RUN_TYPE_LOCAL);
+        } else {
+            taskData.setRunType(TaskData.RUN_TYPE_GLOBAL_RPC);
+        }
 
-	/**
-	 * 远程运行任务，并返回future<TaskData<?,?>>。 如果需要获得数据，可以使用futrue.get()来获得。
-	 * 此方法要谨慎使用，因为task存在限速，大并发下可能会导致线程数超。
-	 * 
-	 * @param runTarget
-	 *            目标主机配置名，如果没有，则为空
-	 * @param taskData
-	 *            任务数据
-	 * @return
-	 */
-	public <TP, RD> Future<TaskData<TP, RD>> runTaskAsync(final TaskData<TP, RD> taskData,
-			final TypeReference<TaskData<TP, RD>> typeRef) {
-		taskData.setId(globalSequenceManager.nextId("task_runner_log"));
-		taskData.setQueueDate(new Date());
+        if (taskData.getRunType() == TaskData.RUN_TYPE_LOCAL) {
+            taskRunnerContainer.process(taskData);
+            return taskData;
+        } else {
+            // 全局运行模式
+            String queue = TaskMetaInfoManager.getFitQueue(taskData);
+            @SuppressWarnings("unchecked")
+            TaskData<TP, RD> retdata = (TaskData<TP, RD>) rabbitTemplate.convertSendAndReceive(queue, queue, taskData,
+                    new MessagePostProcessor() {
 
-		// 当自动RPC，并且本地有runner，而且target匹配的时候，运行在本地模式下。
-		if (taskData.getRunType() == TaskData.RUN_TYPE_AUTO_RPC && TaskMetaInfoManager.checkRunnerRunLocal(taskData)) {
-			// 启动本地运行模式。
-			taskData.setRunType(TaskData.RUN_TYPE_LOCAL);
-		} else {
-			taskData.setRunType(TaskData.RUN_TYPE_GLOBAL_RPC);
-		}
-		if (taskData.getRunType() == TaskData.RUN_TYPE_LOCAL) {
-			// 启动本地运行模式。
-			Future<TaskData<TP, RD>> future = taskRpcService.submit(() -> {
-				taskRunnerContainer.process(taskData);
-				return taskData;
-			});
-			return future;
-		} else {
-			// 全局运行模式
-			Future<TaskData<TP, RD>> future = taskRpcService.submit(() -> {
-				String queue = TaskMetaInfoManager.getFitQueue(taskData);
-				@SuppressWarnings("unchecked")
-				TaskData<TP, RD> retdata = (TaskData<TP, RD>) rabbitTemplate.convertSendAndReceive(queue, queue,
-						taskData, new MessagePostProcessor() {
+                        @Override
+                        public Message postProcessMessage(Message message) throws AmqpException {
+                            MessageProperties mp = message.getMessageProperties();
+                            mp.setPriority(10);
+                            mp.setDeliveryMode(MessageDeliveryMode.NON_PERSISTENT);
+                            mp.setExpiration("180000");
+                            return message;
+                        }
+                    });
+            if (typeRef != null) {
+                try {
+                    retdata = TaskMessageConverter.getTaskObjectMapper().convertValue(retdata, typeRef);
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+            return retdata;
+        }
+    }
 
-							@Override
-							public Message postProcessMessage(Message message) throws AmqpException {
-								MessageProperties mp = message.getMessageProperties();
-								mp.setPriority(10);
-								mp.setDeliveryMode(MessageDeliveryMode.NON_PERSISTENT);
-								mp.setExpiration("180000");
-								return message;
-							}
-						});
-				if (typeRef != null) {
-					try {
-						retdata = TaskMessageConverter.getTaskObjectMapper().convertValue(retdata, typeRef);
-					} catch (Exception e) {
-						log.error(e.getMessage(), e);
-					}
-				}
-				return retdata;
-			});
-			return future;
-		}
-	}
+    /**
+     * 远程运行任务，并返回future<TaskData<?,?>>。 如果需要获得数据，可以使用futrue.get()来获得。
+     * 此方法要谨慎使用，因为task存在限速，大并发下可能会导致线程数超。
+     *
+     * @param runTarget 目标主机配置名，如果没有，则为空
+     * @param taskData  任务数据
+     * @return
+     */
+    public <TP, RD> Future<TaskData<TP, RD>> runTaskAsync(final TaskData<TP, RD> taskData,
+                                                          final TypeReference<TaskData<TP, RD>> typeRef) {
+        taskData.setId(globalSequenceManager.nextId("task_runner_log"));
+        taskData.setQueueDate(new Date());
+
+        // 当自动RPC，并且本地有runner，而且target匹配的时候，运行在本地模式下。
+        if (taskData.getRunType() == TaskData.RUN_TYPE_AUTO_RPC && TaskMetaInfoManager.checkRunnerRunLocal(taskData)) {
+            // 启动本地运行模式。
+            taskData.setRunType(TaskData.RUN_TYPE_LOCAL);
+        } else {
+            taskData.setRunType(TaskData.RUN_TYPE_GLOBAL_RPC);
+        }
+        if (taskData.getRunType() == TaskData.RUN_TYPE_LOCAL) {
+            // 启动本地运行模式。
+            Future<TaskData<TP, RD>> future = taskRpcService.submit(() -> {
+                taskRunnerContainer.process(taskData);
+                return taskData;
+            });
+            return future;
+        } else {
+            // 全局运行模式
+            Future<TaskData<TP, RD>> future = taskRpcService.submit(() -> {
+                String queue = TaskMetaInfoManager.getFitQueue(taskData);
+                @SuppressWarnings("unchecked")
+                TaskData<TP, RD> retdata = (TaskData<TP, RD>) rabbitTemplate.convertSendAndReceive(queue, queue,
+                        taskData, new MessagePostProcessor() {
+
+                            @Override
+                            public Message postProcessMessage(Message message) throws AmqpException {
+                                MessageProperties mp = message.getMessageProperties();
+                                mp.setPriority(10);
+                                mp.setDeliveryMode(MessageDeliveryMode.NON_PERSISTENT);
+                                mp.setExpiration("180000");
+                                return message;
+                            }
+                        });
+                if (typeRef != null) {
+                    try {
+                        retdata = TaskMessageConverter.getTaskObjectMapper().convertValue(retdata, typeRef);
+                    } catch (Exception e) {
+                        log.error(e.getMessage(), e);
+                    }
+                }
+                return retdata;
+            });
+            return future;
+        }
+    }
 
 }
--- src/main/java/uw/task/api/TaskAPI.java
+++ src/main/java/uw/task/api/TaskAPI.java
@@ -1,26 +1,20 @@
 package uw.task.api;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+import uw.task.TaskData;
+import uw.task.conf.TaskProperties;
+import uw.task.entity.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.client.RestTemplate;
-
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
-import uw.task.TaskData;
-import uw.task.conf.TaskProperties;
-import uw.task.entity.TaskContact;
-import uw.task.entity.TaskCronerConfig;
-import uw.task.entity.TaskCronerLog;
-import uw.task.entity.TaskHostStatus;
-import uw.task.entity.TaskRunnerConfig;
 
 /**
  * 对应服务器端的API接口实现。
@@ -29,232 +23,229 @@ import uw.task.entity.TaskRunnerConfig;
  */
 public class TaskAPI {
 
-	private static final Logger log = LoggerFactory.getLogger(TaskAPI.class);
+    private static final Logger log = LoggerFactory.getLogger(TaskAPI.class);
 
-	/**
-	 * Task配置文件
-	 */
-	private TaskProperties taskProperties;
+    /**
+     * Task配置文件
+     */
+    private TaskProperties taskProperties;
 
-	/**
-	 * Rest模板类
-	 */
-	private RestTemplate restTemplate;
+    /**
+     * Rest模板类
+     */
+    private RestTemplate restTemplate;
 
-	/**
-	 * 专门给日志发送使用的线程池。
-	 */
-	private ExecutorService taskLogService = null;
+    /**
+     * 专门给日志发送使用的线程池。
+     */
+    private ExecutorService taskLogService = null;
 
-	/**
-	 * 本机的外网IP
-	 */
-	private String hostIp = "";
+    /**
+     * 本机的外网IP
+     */
+    private String hostIp = "";
 
-	public TaskAPI(final TaskProperties taskProperties, final RestTemplate restTemplate) {
-		this.taskProperties = taskProperties;
-		this.restTemplate = restTemplate;
-		this.taskLogService = new ThreadPoolExecutor(taskProperties.getTaskLogMinThreadNum(), taskProperties.getTaskLogMaxThreadNum(), 20L, TimeUnit.SECONDS,
-				new LinkedBlockingQueue<Runnable>(),
-				new ThreadFactoryBuilder().setDaemon(true).setNameFormat("TaskLog-%d").build());
-	}
+    public TaskAPI(final TaskProperties taskProperties, final RestTemplate restTemplate) {
+        this.taskProperties = taskProperties;
+        this.restTemplate = restTemplate;
+        this.taskLogService = new ThreadPoolExecutor(taskProperties.getTaskLogMinThreadNum(), taskProperties.getTaskLogMaxThreadNum(), 20L, TimeUnit.SECONDS,
+                new SynchronousQueue<>(),
+                new ThreadFactoryBuilder().setDaemon(true).setNameFormat("TaskLog-%d").build(), new ThreadPoolExecutor.CallerRunsPolicy());
+    }
 
-	/**
-	 * @return the hostIp
-	 */
-	public String getHostIp() {
-		return hostIp;
-	}
+    /**
+     * @return the hostIp
+     */
+    public String getHostIp() {
+        return hostIp;
+    }
 
-	/**
-	 * 更新当前主机目标配置。
-	 *
-	 * @return
-	 */
-	public List<String> getServerTargetConfig() {
-		List<String> targetConfig = null;
-		// 判断是否是私有模式。
-		boolean privacyMode = taskProperties.isPrivacyMode();
-		try {
-			String[] data = restTemplate.getForObject(
-					taskProperties.getServerHost() + "/taskapi/target/config?targetType={targetType}", String[].class,
-					privacyMode ? 1 : 0);
-			targetConfig = Arrays.asList(data);
-		} catch (Exception e) {
-			log.error("TaskAPI.getHostConfig()服务端Target配置拉取异常:{}", e.getMessage());
-		}
-		if (targetConfig == null || targetConfig.size() == 0) {
-			if (log.isWarnEnabled()) {
-				log.warn("获得主机Target服务端配置失败，启用默认配置项!");
-			}
-			targetConfig = new ArrayList<>();
-			if (!privacyMode) {
-				targetConfig.add("");
-			}
-		}
-		return targetConfig;
-	}
+    /**
+     * 更新当前主机目标配置。
+     *
+     * @return
+     */
+    public List<String> getServerTargetConfig() {
+        List<String> targetConfig = null;
+        // 判断是否是私有模式。
+        boolean privacyMode = taskProperties.isPrivacyMode();
+        try {
+            String[] data = restTemplate.getForObject(
+                    taskProperties.getServerHost() + "/taskapi/target/config?targetType={targetType}", String[].class,
+                    privacyMode ? 1 : 0);
+            targetConfig = Arrays.asList(data);
+        } catch (Exception e) {
+            log.error("TaskAPI.getHostConfig()服务端Target配置拉取异常:{}", e.getMessage());
+        }
+        if (targetConfig == null || targetConfig.size() == 0) {
+            if (log.isWarnEnabled()) {
+                log.warn("获得主机Target服务端配置失败，启用默认配置项!");
+            }
+            targetConfig = new ArrayList<>();
+            if (!privacyMode) {
+                targetConfig.add("");
+            }
+        }
+        return targetConfig;
+    }
 
-	/**
-	 * 更新当前主机状态，返回主机IP地址。
-	 *
-	 * @return 主机IP地址
-	 */
-	public String updateHostStatus() {
-		String ip = "";
-		TaskHostStatus taskHostStatus = new TaskHostStatus();
-		taskHostStatus.setHostId(taskProperties.getHostId());
-		taskHostStatus.setTaskProject(taskProperties.getProject());
-		try {
-			ip = restTemplate.postForObject(taskProperties.getServerHost() + "/taskapi/host/status", taskHostStatus,
-					String.class);
-			hostIp = ip;
-		} catch (Exception e) {
-			log.error("TaskAPI.updateHostStatus()服务端主机状态更新异常:{}", e.getMessage());
-		}
-		return ip;
-	}
+    /**
+     * 更新当前主机状态，返回主机IP地址。
+     *
+     * @return 主机IP地址
+     */
+    public String updateHostStatus() {
+        String ip = "";
+        TaskHostStatus taskHostStatus = new TaskHostStatus();
+        taskHostStatus.setHostId(taskProperties.getHostId());
+        taskHostStatus.setTaskProject(taskProperties.getProject());
+        try {
+            ip = restTemplate.postForObject(taskProperties.getServerHost() + "/taskapi/host/status", taskHostStatus,
+                    String.class);
+            hostIp = ip;
+        } catch (Exception e) {
+            log.error("TaskAPI.updateHostStatus()服务端主机状态更新异常:{}", e.getMessage());
+        }
+        return ip;
+    }
 
-	/**
-	 * 初始化CronerConfig。
-	 *
-	 * @param config
-	 */
-	public TaskCronerConfig initTaskCronerConfig(TaskCronerConfig config) {
-		try {
-			config = restTemplate.postForObject(taskProperties.getServerHost() + "/taskapi/croner/config", config,
-					TaskCronerConfig.class);
-		} catch (Exception e) {
-			log.error("TaskAPI.initTaskCronerConfig上传Croner配置到服务端异常:{}", e.getMessage());
-		}
-		return config;
-	}
+    /**
+     * 初始化CronerConfig。
+     *
+     * @param config
+     */
+    public TaskCronerConfig initTaskCronerConfig(TaskCronerConfig config) {
+        try {
+            config = restTemplate.postForObject(taskProperties.getServerHost() + "/taskapi/croner/config", config,
+                    TaskCronerConfig.class);
+        } catch (Exception e) {
+            log.error("TaskAPI.initTaskCronerConfig上传Croner配置到服务端异常:{}", e.getMessage());
+        }
+        return config;
+    }
 
-	/**
-	 * 初始化RunnerConfig
-	 *
-	 * @param config
-	 */
-	public TaskRunnerConfig initTaskRunnerConfig(TaskRunnerConfig config) {
-		try {
-			config = restTemplate.postForObject(taskProperties.getServerHost() + "/taskapi/runner/config", config,
-					TaskRunnerConfig.class);
-		} catch (Exception e) {
-			log.error("TaskAPI.initTaskRunnerConfig上传Runner配置到服务端异常:{}", e.getMessage());
-		}
-		return config;
+    /**
+     * 初始化RunnerConfig
+     *
+     * @param config
+     */
+    public TaskRunnerConfig initTaskRunnerConfig(TaskRunnerConfig config) {
+        try {
+            config = restTemplate.postForObject(taskProperties.getServerHost() + "/taskapi/runner/config", config,
+                    TaskRunnerConfig.class);
+        } catch (Exception e) {
+            log.error("TaskAPI.initTaskRunnerConfig上传Runner配置到服务端异常:{}", e.getMessage());
+        }
+        return config;
 
-	}
+    }
 
-	/**
-	 * 初始化联系人信息。
-	 *
-	 * @param contact
-	 */
-	public void initTaskContact(TaskContact contact) {
-		try {
-			restTemplate.postForLocation(taskProperties.getServerHost() + "/taskapi/contact", contact);
-		} catch (Exception e) {
-			log.error("TaskAPI.initTaskContact上传联系人信息到服务端异常:{}", e.getMessage());
-		}
-	}
+    /**
+     * 初始化联系人信息。
+     *
+     * @param contact
+     */
+    public void initTaskContact(TaskContact contact) {
+        try {
+            restTemplate.postForLocation(taskProperties.getServerHost() + "/taskapi/contact", contact);
+        } catch (Exception e) {
+            log.error("TaskAPI.initTaskContact上传联系人信息到服务端异常:{}", e.getMessage());
+        }
+    }
 
-	/**
-	 * 根据包名前缀获得TaskRunner队列列表。
-	 *
-	 * @param lastUpdateTime
-	 *            最后更新时间
-	 * @return
-	 */
-	public List<TaskRunnerConfig> getTaskRunnerQueueList(long lastUpdateTime) {
-		List<TaskRunnerConfig> list = null;
-		try {
-			TaskRunnerConfig[] data = restTemplate.getForObject(
-					taskProperties.getServerHost() + "/taskapi/runner/queue?lastUpdateTime={lastUpdateTime}",
-					TaskRunnerConfig[].class, lastUpdateTime);
-			list = Arrays.asList(data);
-		} catch (Exception e) {
-			log.error("TaskAPI.getTaskRunnerQueueList()服务端主机状态更新异常:{}", e.getMessage());
-		}
+    /**
+     * 根据包名前缀获得TaskRunner队列列表。
+     *
+     * @param lastUpdateTime 最后更新时间
+     * @return
+     */
+    public List<TaskRunnerConfig> getTaskRunnerQueueList(long lastUpdateTime) {
+        List<TaskRunnerConfig> list = null;
+        try {
+            TaskRunnerConfig[] data = restTemplate.getForObject(
+                    taskProperties.getServerHost() + "/taskapi/runner/queue?lastUpdateTime={lastUpdateTime}",
+                    TaskRunnerConfig[].class, lastUpdateTime);
+            list = Arrays.asList(data);
+        } catch (Exception e) {
+            log.error("TaskAPI.getTaskRunnerQueueList()服务端主机状态更新异常:{}", e.getMessage());
+        }
 
-		return list;
-	}
+        return list;
+    }
 
-	/**
-	 * 根据包名前缀获得TaskRunner配置列表。
-	 *
-	 * @param taskPackage
-	 * @param lastUpdateTime
-	 *            最后更新时间
-	 * @return
-	 */
-	public List<TaskRunnerConfig> getTaskRunnerConfigList(String taskPackage, long lastUpdateTime) {
-		List<TaskRunnerConfig> list = null;
-		try {
-			TaskRunnerConfig[] data = restTemplate.getForObject(
-					taskProperties.getServerHost()
-							+ "/taskapi/runner/config?taskPackage={taskPackage}&lastUpdateTime={lastUpdateTime}",
-					TaskRunnerConfig[].class, taskPackage, lastUpdateTime);
-			list = Arrays.asList(data);
-		} catch (Exception e) {
-			log.error("TaskAPI.getTaskRunnerConfigList()服务端主机状态更新异常:{}", e.getMessage());
-		}
+    /**
+     * 根据包名前缀获得TaskRunner配置列表。
+     *
+     * @param taskPackage
+     * @param lastUpdateTime 最后更新时间
+     * @return
+     */
+    public List<TaskRunnerConfig> getTaskRunnerConfigList(String taskPackage, long lastUpdateTime) {
+        List<TaskRunnerConfig> list = null;
+        try {
+            TaskRunnerConfig[] data = restTemplate.getForObject(
+                    taskProperties.getServerHost()
+                            + "/taskapi/runner/config?taskPackage={taskPackage}&lastUpdateTime={lastUpdateTime}",
+                    TaskRunnerConfig[].class, taskPackage, lastUpdateTime);
+            list = Arrays.asList(data);
+        } catch (Exception e) {
+            log.error("TaskAPI.getTaskRunnerConfigList()服务端主机状态更新异常:{}", e.getMessage());
+        }
 
-		return list;
-	}
+        return list;
+    }
 
-	/**
-	 * 根据包名前缀获得TaskCroner配置列表。
-	 *
-	 * @param taskPackage
-	 * @param lastUpdateTime
-	 *            最后更新时间
-	 * @return
-	 */
-	public List<TaskCronerConfig> getTaskCronerConfigList(String taskPackage, long lastUpdateTime) {
-		List<TaskCronerConfig> list = null;
-		try {
-			TaskCronerConfig[] data = restTemplate.getForObject(
-					taskProperties.getServerHost()
-							+ "/taskapi/croner/config?taskPackage={taskPackage}&lastUpdateTime={lastUpdateTime}",
-					TaskCronerConfig[].class, taskPackage, lastUpdateTime);
-			list = Arrays.asList(data);
-		} catch (Exception e) {
-			log.error("TaskAPI.getTaskCronerConfigList()服务端主机状态更新异常:{}", e.getMessage());
-		}
+    /**
+     * 根据包名前缀获得TaskCroner配置列表。
+     *
+     * @param taskPackage
+     * @param lastUpdateTime 最后更新时间
+     * @return
+     */
+    public List<TaskCronerConfig> getTaskCronerConfigList(String taskPackage, long lastUpdateTime) {
+        List<TaskCronerConfig> list = null;
+        try {
+            TaskCronerConfig[] data = restTemplate.getForObject(
+                    taskProperties.getServerHost()
+                            + "/taskapi/croner/config?taskPackage={taskPackage}&lastUpdateTime={lastUpdateTime}",
+                    TaskCronerConfig[].class, taskPackage, lastUpdateTime);
+            list = Arrays.asList(data);
+        } catch (Exception e) {
+            log.error("TaskAPI.getTaskCronerConfigList()服务端主机状态更新异常:{}", e.getMessage());
+        }
 
-		return list;
-	}
+        return list;
+    }
 
-	/**
-	 * 发送Runner任务日志。
-	 *
-	 * @param taskData
-	 */
-	public void sendTaskRunnerLog(TaskData<?, ?> taskData) {
-		taskLogService.submit(() -> {
-			try {
-				restTemplate.postForLocation(taskProperties.getServerHost() + "/taskapi/runner/log", taskData);
-			} catch (Exception e) {
-				log.error("TaskAPI.sendTaskRunnerLog日志发送到服务端异常:{}", e.getMessage());
-			}
-		});
+    /**
+     * 发送Runner任务日志。
+     *
+     * @param taskData
+     */
+    public void sendTaskRunnerLog(TaskData<?, ?> taskData) {
+        taskLogService.submit(() -> {
+            try {
+                restTemplate.postForLocation(taskProperties.getServerHost() + "/taskapi/runner/log", taskData);
+            } catch (Exception e) {
+                log.error("TaskAPI.sendTaskRunnerLog日志发送到服务端异常:{}", e.getMessage());
+            }
+        });
 
-	}
+    }
 
-	/**
-	 * 发送CronLog日志。
-	 *
-	 * @param taskCronerLog
-	 */
-	public void sendTaskCronerLog(TaskCronerLog taskCronerLog) {
-		taskLogService.submit(() -> {
-			try {
-				restTemplate.postForLocation(taskProperties.getServerHost() + "/taskapi/croner/log", taskCronerLog);
-			} catch (Exception e) {
-				log.error("TaskAPI.sendTaskCronerLog日志发送到服务端异常:{}", e.getMessage());
-			}
-		});
-	}
+    /**
+     * 发送CronLog日志。
+     *
+     * @param taskCronerLog
+     */
+    public void sendTaskCronerLog(TaskCronerLog taskCronerLog) {
+        taskLogService.submit(() -> {
+            try {
+                restTemplate.postForLocation(taskProperties.getServerHost() + "/taskapi/croner/log", taskCronerLog);
+            } catch (Exception e) {
+                log.error("TaskAPI.sendTaskCronerLog日志发送到服务端异常:{}", e.getMessage());
+            }
+        });
+    }
 
 }
